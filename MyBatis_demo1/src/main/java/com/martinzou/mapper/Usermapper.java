9287eea7179977931cee13d297a4d1252edba24f
MartinPC <1063064876@qq.com>
2023-09-06 00:11:03 +0800

    Mybatis项目POM文件以及实体类创建

---
+++ MyBatis_demo1/src/main/java/com/martinzou/mapper/Usermapper.java
@@ -0,0 +1,9 @@
+package com.martinzou.mapper;
+
+public interface Usermapper {
+
+    /*
+    * 添加用户信息
+    * */
+    int insertUser();
+}
---
+++ MyBatis_demo1/src/main/java/com/martinzou/pojo/User.java
@@ -0,0 +1,24 @@
+package com.martinzou.pojo;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private Integer id;
+
+    private String userName;
+
+    private String password;
+
+    private Integer age;
+
+    private String gender;
+
+    private String email;
+
+}
